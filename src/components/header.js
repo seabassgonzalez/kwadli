85e80710a524adfa67c1d68342e63edff29a4048
seabassgonzalez <seabassgonzalez@gmail.com>
2018-05-10 16:57:31 -0700

    Initial header component, simple nav with ul and sign in li

---
+++ src/components/header.js
@@ -0,0 +1,27 @@
+// import React and Component from react
+
+// class Header extends Component
+	// render
+		// return
+			// nav bootstrap classes navbar navbar-light
+				// ul bootstrap classes nav navbar-nav
+					// li classname nav-item
+						// simple sign in text
+
+// export default Header					
+
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render(){
+		return(
+			<nav>
+				<ul>
+					<li></li>
+				</ul>
+			</nav>
+		);
+	}
+}
+
+export default Header;
