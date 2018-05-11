542cdd1be6bbfe7b8a0bbb9406e412437b5b7d4a
seabassgonzalez <seabassgonzalez@gmail.com>
2018-05-11 13:55:07 -0700

    Update form using Field component as per docs, track props using console messages

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,5 +1,5 @@
 // import Rect and Component from react
-// import redux form from redux-form
+// import redux form and Field from redux-form
 
 // class Signin extends Component
 	// handleFormSubmit taking email and password
@@ -7,23 +7,25 @@
 	// render
 		// const to store fields and password onto this.props using handle submit helper from redux form
 		//return 
-			// form with onsubmit handler that calls handleSubmit calling this.handleFormSubmit bound to this instance
+			// form with onsubmit handler that calls handleSubmit calling this.handleFormSubmit bound to this instance - handleSubmit puts props on fields and passes to handleFormSubmit
 				// field set component with classname form-group
 					// email label
-					//  input taking email helper bootstrap classname form-control
+					//  Field with name taking email as input component with  bootstrap classname form-control
 				// field set component with classname form-group
 					// password label
-					//  input taking password helper spread operator password bootstrap classname form-control
+					//  Field with name taking password as input component with bootstrap classname form-control
 				// button action submit classname btn btn-primary with text Sign in
 
 // export default reduxForm(configuration)(component) -- object with property form set to name of form, fields to produce including email and password, and Signin as component
 
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form'; 
+import { reduxForm, Field } from 'redux-form'; 
 
 class Signin extends Component {
 	handleFormSubmit({ email, password }){
 		console.log(email, password);
+		console.log('props are ', this.props);
+		console.log('fields are ', this.props.fields);
 	}
 	render(){
 		const { handleSubmit, fields: { email, password }} = this.props;
@@ -31,11 +33,11 @@ class Signin extends Component {
 			<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
 				<fieldset className="form-group">
 					<label>Email:</label>
-					<input {...email} className="form-control" />
+					<Field name="email" component="input" className="form-control" />
 				</fieldset>
 				<fieldset className="form-group">
 					<label>Password:</label>
-					<input {...password} className="form-control" />
+					<Field name="password" component="input" className="form-control" />
 				</fieldset>
 				<button action="submit" className="btn btn-primary">Sign in</button>
 			</form>
