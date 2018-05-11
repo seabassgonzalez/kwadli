969678924805f4a2b1ff651cfdf1b3b0f937c5ae
seabassgonzalez <seabassgonzalez@gmail.com>
2018-05-10 17:31:26 -0700

    Create sign in form using form fieldsets for email and password, set form piece of state to reducer

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,15 +1,37 @@
 // import Rect and Component from react
+// import redux form from redux-form
 
 // class Signin extends Component
 	// render
-		
+		// form
+			// field set component with classname form-group
+				// email label
+				//  input bootstrap classname form-control
+			// field set component with classname form-group
+				// password label
+				//  input bootstrap classname form-control
+			// button action submit classname btn btn-primary with text Sign in
+
 // export default reduxForm(configuration)(component) -- object with property form set to name of form, fields to produce including email and password, and Signin as component
 
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form'; 
 
 class Signin extends Component {
 	render(){
-
+		return(
+			<form>
+				<fieldset className="form-group">
+					<label>Email:</label>
+					<input className="form-control" />
+				</fieldset>
+				<fieldset className="form-group">
+					<label>Password:</label>
+					<input className="form-control" />
+				</fieldset>
+				<button action="submit" className="btn btn-primary">Sign in</button>
+			</form>
+		);
 	}
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,16 @@
 // import combineReducers from redux
+// import reducer from redux-form
 
 // create const rootReducer set to call to combineReducers 
-	// dummy state set to (state = {} )=> state
+	// set form property of state to reducer
 
 // export default rootReducer
 
 import { combineReducers } from 'redux';
+import { reducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  form: reducer
 });
 
 export default rootReducer;
