47118f969165df8c7f9ce1807bf843309f34dd10
seabassgonzalez <seabassgonzalez@gmail.com>
2018-05-12 15:57:25 -0700

    Implement js promise to handle route forwarding when signin request successful

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,31 @@
 // import axios from axios
+// import browserHistory from react-router
 
 // store root url for api server url
 
 // export function signinUser that takes email and password properties
 	// return a function from this action creator instead of just object passing it dispatch method, both made available by thunk to access dispatch
 		// submit email, password to server using axios.post() request passing it url we want to post and data to post, {email, password} becomes { email: email, password: password } through es6
-		// if request is good
-			// udpate state to indicate user is authenticated
-			// save JWT token
-			// redirect to route '/feature'
-		// if request is bad
-			// show error
+			// use .then() promise construct if request is good with a response
+				// udpate state to indicate user is authenticated
+				// save JWT token
+				// redirect to route '/feature'
+			// use .catch() promise construct if request is bad
+				// show error
 
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }){
 	return function(dispatch){
-		axios.post(`${ROOT_URL}/signin`, { email, password });
+		axios.post(`${ROOT_URL}/signin`, { email, password })
+			.then(response => {
+				browserHistory.push('/feature');
+			})
+			.catch(() => {
+
+			});
 	}
 }		
