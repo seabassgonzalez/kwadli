458d98012d36317b19d25f7538eb9cbc7ad79cf0
seabassgonzalez <seabassgonzalez@gmail.com>
2018-05-12 16:10:12 -0700

    Initial reducer to handle auth state, included in main reducer index file

---
+++ src/reducers/auth_reducer.js
@@ -0,0 +1,11 @@
+// to maintain auth state
+// export function taking state object and action
+	// switch() over action.type
+	// return state
+
+	export default function(state = {}, action){
+		switch(action.type){
+
+		}
+		return state;
+	}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,19 @@
 // import combineReducers from redux
 // import reducer from redux-form
+// import authReducer from auth_reducer
 
 // create const rootReducer set to call to combineReducers 
 	// set form property of state to reducer
-
+	// auth set to authReducer
 // export default rootReducer
 
 import { combineReducers } from 'redux';
 import { reducer } from 'redux-form';
+import authReducer from './auth_reducer';
 
 const rootReducer = combineReducers({
-  form: reducer
+  form: reducer,
+  auth: authReducer
 });
 
 export default rootReducer;
