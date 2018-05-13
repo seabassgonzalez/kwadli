13f14579453076f6caefa8108c027763c192b7ec
seabassgonzalez <seabassgonzalez@gmail.com>
2018-05-13 14:08:07 -0700

    Initial signout component providing an opportunity to share more information with user as they leave

---
+++ src/components/auth/signout.js
@@ -0,0 +1,17 @@
+// import React and Component from react
+// import connect from react-redux
+// import all actions as actions from actions
+
+// class Signout component extends Component
+	// render
+		// return div with signout message
+
+import React, { React } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Signout extends Component {
+	render() { 
+		return <div>See you next time</div>;
+	}
+}
